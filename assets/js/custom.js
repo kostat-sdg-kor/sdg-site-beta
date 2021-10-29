afd7354de95a1f2bcb31cee2373e122025a14f0c
kostat-sdg-kor <46581861+kostat-sdg-kor@users.noreply.github.com>
2021-10-29 04:26:37 +0000

    Deploying to gh-pages from  @ b9278781f524163c7f358184439dcb3b2cb22294 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,12 +10,12 @@ function zoomOut(){
     zooms();
 }
 function zoomIn(){
-    nowZoom = nowZoom + 0.1;
-    if(nowZoom >= 3) nowZoom = 3;
+    nowZoom = nowZoom + 0.2;
+    if(nowZoom >= 1.6) nowZoom = 1.6;
     zooms();
 }
 function zooms(){
     document.body.style.zoom = nowZoom;
     if(nowZoom <= 0.7) alert(" 더 이상 축소할 수 없습니다.")
-    if(nowZoom >= 3) alert(" 더 이상 확대할 수 없습니다.")
+    if(nowZoom >=1.6) alert(" 더 이상 확대할 수 없습니다.")
 }
